cc43cf69d4521fb88532d82aabeaf5a0da78284c
Shahana Salam <shahana@cxunicorn.com>
2023-05-16 15:11:03 +0400

    Project setup

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & share AI prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient"></div>
+        </div>
+
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
